e00d359e248d1c408dd60523d95db62d9375a7f3
Borbala Mester <mest0006@alognquinlive.com>
2020-05-25 09:36:12 -0400

    update

--- script.js
+++ script.js
@@ -12,14 +12,21 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 
 
-let hrPosition = 20;
-let minPosition = min * 360 / 60;;
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
 let secPosition = sec * 360 / 60;
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg )";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg )";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg )";
+function runTheClock () {
 
-let b = c;
+  hrPosition = hrPosition + (3 / 360);
+  minPosition = minPosition + (6 / 60);
+  secPosition = secPosition + 6;
 
+  HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
 
